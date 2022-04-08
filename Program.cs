e5c8045aee19335bff1fe2f701605c3af3bf0e7f
Siddhant <santoshshrivastava39@gmail.com>
2022-04-08 16:56:04 +0530

    Call in main method

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace MoodAnalyserProblems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Are you happy or sad?");
+
+            MoodAnalyser value = new MoodAnalyser("Happy");
+           string result= value.AnalyseMood();
+
+            Console.WriteLine(result);
+            
+        }
+    }
+}
+
