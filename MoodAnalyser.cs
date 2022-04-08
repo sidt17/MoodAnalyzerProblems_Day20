a150dc8f9a4b62b22815ebdd8b744d4b2b60003d
Siddhant <santoshshrivastava39@gmail.com>
2022-04-08 16:57:36 +0530

    Analyse and respond Happy or Sad

---
+++ MoodAnalyser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyserProblems
+{
+    public class MoodAnalyser
+    {
+
+        public string msg;
+        public MoodAnalyser(string msg)
+        {
+            this.msg = msg;
+        }
+
+        public string AnalyseMood()
+        {
+            if (msg.ToLower().Contains("sad"))
+                return "SAD";
+
+            else
+                return "HAPPY";
+        }
+    }
+}
